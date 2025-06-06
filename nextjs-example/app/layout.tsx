9350ef67886e33a649a8bcbee21886c512812432
Kien Dinh <kien.dinh@asnet.com.vn>
2025-06-06 17:27:50 +0700

    feat: update examples metadata for chapter 16: Adding Metadata

--- nextjs-example/app/layout.tsx
+++ nextjs-example/app/layout.tsx
@@ -1,14 +1,20 @@
 import "@/app/ui/global.css";
 import { inter } from "@/app/ui/fonts";
+import { Metadata } from "next";
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
-    </html>
-  );
-}
+export const metadata: Metadata = {
+  title: {
+    template: "%s | Acme Dashboard",
+    default: "Acme Dashboard",
+  },
+  description: "The website provides examples related to the Next.js course",
+  metadataBase: new URL("https://next-learn-dashboard.vercel.sh"),
+};
+
+const RootLayout = ({ children }: { children: React.ReactNode }) => (
+  <html lang="en">
+    <body className={`${inter.className} antialiased`}>{children}</body>
+  </html>
+);
+
+export default RootLayout;
