752ad9b99569d2c11e260478f135ef9908529962
Kien Dinh <kien.dinh@asnet.com.vn>
2025-06-06 15:18:16 +0700

    feat: update examples about edit form for chapter 14: Improving Accessibility

---
+++ nextjs-example/app/dashboard/invoices/[id]/edit/page.tsx
@@ -0,0 +1,25 @@
+import { fetchInvoiceById, fetchCustomers } from "@/app/lib/data";
+import { notFound } from "next/navigation";
+import EditInvoiceForm from "@/app/ui/invoices/edit-form";
+
+const EditInvoicePage = async ({ params }: { params: { id: string } }) => {
+  const id = await Promise.resolve(params.id);
+
+  const [invoice, customers] = await Promise.all([
+    fetchInvoiceById(id),
+    fetchCustomers(),
+  ]);
+
+  if (!invoice) {
+    notFound();
+  }
+
+  return (
+    <main className="max-w-2xl mx-auto p-6">
+      <h1 className="text-2xl font-bold mb-4">Edit Invoice</h1>
+      <EditInvoiceForm invoice={invoice} customers={customers} />
+    </main>
+  );
+};
+
+export default EditInvoicePage;
