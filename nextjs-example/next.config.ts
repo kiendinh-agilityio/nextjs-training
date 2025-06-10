66830203a7e64169254188bd6abab540a32f153a
Kien Dinh <kien.dinh@asnet.com.vn>
2025-06-10 17:31:36 +0700

    feat: config next.config.ts examples for content security policy

--- nextjs-example/next.config.ts
+++ nextjs-example/next.config.ts
@@ -1,8 +1,34 @@
-import type { NextConfig } from 'next';
+import { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['www.muchbetteradventures.com'],
+    domains: ["www.muchbetteradventures.com"],
+  },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: `
+              default-src 'self';
+              script-src 'self' 'unsafe-eval' 'unsafe-inline';
+              style-src 'self' 'unsafe-inline';
+              img-src 'self' blob: data:;
+              font-src 'self';
+              object-src 'none';
+              base-uri 'self';
+              form-action 'self';
+              frame-ancestors 'none';
+              upgrade-insecure-requests;
+            `
+              .replace(/\s{2,}/g, " ")
+              .trim(),
+          },
+        ],
+      },
+    ];
   },
 };
 
