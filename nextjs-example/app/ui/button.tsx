c30a5e236948e695db33706e36e4f10305a7f8bf
Kien Dinh <kien.dinh@asnet.com.vn>
2025-06-13 17:35:09 +0700

    feat: apply examples component Button of framework Shadcn/ui for Invoices page

--- nextjs-example/app/ui/button.tsx
+++ nextjs-example/app/ui/button.tsx
@@ -2,15 +2,26 @@ import clsx from 'clsx';
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
+  variant?: 'default' | 'outline';
 }
 
-export function Button({ children, className, ...rest }: ButtonProps) {
+export function Button({
+  children,
+  className,
+  variant = 'default',
+  ...rest
+}: ButtonProps) {
   return (
     <button
       {...rest}
       className={clsx(
-        'flex h-10 items-center rounded-lg bg-blue-500 px-4 text-sm font-medium text-white transition-colors hover:bg-blue-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-500 active:bg-blue-600 aria-disabled:cursor-not-allowed aria-disabled:opacity-50',
-        className,
+        'flex h-10 items-center rounded-lg px-4 text-sm font-medium transition-colors focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-500 active:bg-blue-600 aria-disabled:cursor-not-allowed aria-disabled:opacity-50',
+        {
+          'bg-blue-500 text-white hover:bg-blue-400': variant === 'default',
+          'border border-gray-200 bg-white text-gray-600 hover:bg-gray-100':
+            variant === 'outline',
+        },
+        className
       )}
     >
       {children}
