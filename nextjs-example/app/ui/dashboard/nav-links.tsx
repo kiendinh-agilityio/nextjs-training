9814e251cf3f2d0dec51d6d34d36a9081910b1b3
Kien Dinh <kien.dinh@asnet.com.vn>
2025-06-16 17:06:05 +0700

    feat: update examples component nav-links for nextjs

--- nextjs-example/app/ui/dashboard/nav-links.tsx
+++ nextjs-example/app/ui/dashboard/nav-links.tsx
@@ -6,6 +6,7 @@ import {
   DocumentDuplicateIcon,
   FolderIcon,
   ArchiveBoxIcon,
+  CloudIcon,
 } from "@heroicons/react/24/outline";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
@@ -21,6 +22,7 @@ const links = [
   { name: "Customers", href: "/dashboard/customers", icon: UserGroupIcon },
   { name: "Blog", href: "/blog", icon: FolderIcon },
   { name: "Products", href: "/products", icon: ArchiveBoxIcon },
+  { name: "Weather", href: "/weather", icon: CloudIcon },
 ];
 
 const NavLinks = () => {
