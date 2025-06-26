85ba5f46f2a8ec758c5a9cb34c0078227b82d46f
Kien Dinh <kien.dinh@asnet.com.vn>
2025-06-26 17:53:52 +0700

    feat: config package.json

--- nextjs-practices/practice-one/tailwind.config.ts
+++ nextjs-practices/practice-one/tailwind.config.ts
@@ -5,11 +5,6 @@ const config: Config = {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {
-      borderRadius: {
-        lg: 'var(--radius)',
-        md: 'calc(var(--radius) - 2px)',
-        sm: 'calc(var(--radius) - 4px)',
-      },
       colors: {
         background: 'var(--background)',
         'background-card': 'var(--background-card)',
