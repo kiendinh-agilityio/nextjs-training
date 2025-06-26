da20eb8feb10027db2ce185ccf2529e31e1785bf
Kien Dinh <kien.dinh@asnet.com.vn>
2025-06-26 14:54:30 +0700

    feat: configure theme, global style variables in globals.css and taildwind.config.ts

--- nextjs-practices/practice-one/tailwind.config.ts
+++ nextjs-practices/practice-one/tailwind.config.ts
@@ -1,10 +1,43 @@
 import type { Config } from 'tailwindcss';
 
 const config: Config = {
+  darkMode: ['class'],
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
-    extend: {},
+    extend: {
+      borderRadius: {
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)',
+      },
+      colors: {
+        background: 'var(--background)',
+        'background-card': 'var(--background-card)',
+        primary: 'var(--primary)',
+        secondary: 'var(--secondary)',
+        neutral: 'var(--neutral)',
+        muted: 'var(--muted)',
+        accent: 'var(--accent)',
+        success: 'var(--success)',
+        error: 'var(--error)',
+      },
+      borderColor: {
+        base: 'var(--border-base)',
+      },
+      boxShadow: {
+        base: 'var(--shadow-base)',
+      },
+      fontFamily: {
+        base: ['Poppins', 'sans-serif'],
+      },
+      fontWeight: {
+        base: '400',
+        medium: '500',
+        semiBold: '600',
+        bold: '700',
+      },
+    },
   },
-  plugins: [],
+  plugins: [require('tailwindcss-animate')],
 };
 export default config;
