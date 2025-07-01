b371fca1d6b8139703c24dc40e4be34f837b7de6
Kien Dinh <kien.dinh@asnet.com.vn>
2025-07-02 00:00:18 +0700

    feat: update contants data for home pages

--- nextjs-practices/practice-one/src/constants/data.ts
+++ nextjs-practices/practice-one/src/constants/data.ts
@@ -21,3 +21,63 @@ export const PROMOTIONS_DATA = [
     image: '/images/promotions-image-one.webp',
   },
 ];
+
+export const POPULAR_CATEGORIES_DATA = [
+  {
+    name: 'Burgers & Fast food',
+    count: 21,
+    image: '/images/burgers-fast.webp',
+  },
+  {
+    name: 'Salads',
+    count: 32,
+    image: '/images/salads.webp',
+  },
+  {
+    name: 'Pasta & Casuals',
+    count: 4,
+    image: '/images/pasta-casuals.webp',
+  },
+  {
+    name: 'Pizza',
+    count: 32,
+    image: '/images/pizza.webp',
+  },
+  {
+    name: 'Breakfast',
+    count: 4,
+    image: '/images/breakfast.webp',
+  },
+  {
+    name: 'Soups',
+    count: 32,
+    image: '/images/soups.webp',
+  },
+];
+
+export const POPULAR_RESTAURANTS_DATA = [
+  {
+    name: "McDonald's London",
+    image: '/images/mcdonald.webp',
+  },
+  {
+    name: 'Papa Johns',
+    image: '/images/papa-johns.webp',
+  },
+  {
+    name: 'KFC West London',
+    image: '/images/kfc.webp',
+  },
+  {
+    name: 'Texas Chicken',
+    image: '/images/texas-chicken.webp',
+  },
+  {
+    name: 'Burger King',
+    image: '/images/burger-king.webp',
+  },
+  {
+    name: 'Shaurma 1',
+    image: '/images/shaurma.webp',
+  },
+];
