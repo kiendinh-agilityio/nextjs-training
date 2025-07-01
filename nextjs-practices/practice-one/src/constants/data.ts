75b79e0adb16f3c7ef9dec58b4c5a987ac753b2f
Kien Dinh <kien.dinh@asnet.com.vn>
2025-07-01 18:32:05 +0700

    feat: implement constants data

---
+++ nextjs-practices/practice-one/src/constants/data.ts
@@ -0,0 +1,23 @@
+export const PROMOTIONS_DATA = [
+  {
+    id: 1,
+    name: 'Chef Burgers London',
+    type: 'Restaurant',
+    discount: 40,
+    image: '/images/promotions-image-one.webp',
+  },
+  {
+    id: 2,
+    name: 'Grand Ai Cafe London',
+    type: 'Restaurant',
+    discount: 20,
+    image: '/images/promotions-image-two.webp',
+  },
+  {
+    id: 3,
+    name: "Butterbrot Caf'e London",
+    type: 'Restaurant',
+    discount: 17,
+    image: '/images/promotions-image-one.webp',
+  },
+];
