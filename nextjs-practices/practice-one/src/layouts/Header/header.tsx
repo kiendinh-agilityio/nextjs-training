d83bff94841288e44cd36bc970d9e1e44a775c47
Kien Dinh <kien.dinh@asnet.com.vn>
2025-07-01 10:10:49 +0700

    feat: update ui for header layout

--- nextjs-practices/practice-one/src/layouts/Header/header.tsx
+++ nextjs-practices/practice-one/src/layouts/Header/header.tsx
@@ -1,15 +1,13 @@
 import { Logo } from '@/components/common/ui/logo';
-import Navbar from './navbar/navbar';
+import Navbar from './Navbar/Navbar';
 
-const Header = () => {
-  return (
-    <header className="w-full border-b bg-white">
-      <div className="container mx-auto flex items-center justify-between py-3 px-2 md:px-0">
-        <Logo href="/" src="/images/logo.svg" />
-        <Navbar />
-      </div>
-    </header>
-  );
-};
+const Header = () => (
+  <header className="w-full border-b bg-white">
+    <div className="container mx-auto flex items-center justify-between py-3 px-2 md:px-0">
+      <Logo href="/" src="/images/logo.svg" />
+      <Navbar />
+    </div>
+  </header>
+);
 
 export default Header;
