3a6efe2bc7ae3b5cae62f0d2b5b61eebbe91848b
Kien Dinh <kien.dinh@asnet.com.vn>
2025-07-01 18:32:39 +0700

    feat: implement ui for home page

--- nextjs-practices/practice-one/src/app/page.tsx
+++ nextjs-practices/practice-one/src/app/page.tsx
@@ -1,11 +1,10 @@
-import { Heading } from '@/components/common/ui/heading';
+import { HeroSection, PromotionsSection } from '@/components/Home';
 
 const Homepage = () => {
   return (
-    <main className="container mx-auto">
-      <Heading as="h1" size="xl">
-        Feast Your Senses, <span className="text-primary">Fast and Fresh</span>
-      </Heading>
+    <main className="container mx-auto lg:px-0">
+      <HeroSection />
+      <PromotionsSection />
     </main>
   );
 };
---
+++ nextjs-practices/practice-one/src/components/Home/index.ts
@@ -0,0 +1,2 @@
+export { default as HeroSection } from './Hero/HeroSection';
+export { default as PromotionsSection } from './Promotions/PromotionsSection';
