a9a5b8860dd32def7f6cdd43930bcea56501f0b4
Kien Dinh <kien.dinh@asnet.com.vn>
2025-07-02 14:33:30 +0700

    feat: implement icons for Footer

---
+++ nextjs-practices/practice-one/src/components/Icons/index.ts
@@ -0,0 +1,4 @@
+export { default as FacebookIcon } from './FacebookIcon';
+export { default as InstagramIcon } from './InstagramIcon';
+export { default as SnapchatIcon } from './SnapchatIcon';
+export { default as TiktokIcon } from './TiktokIcon';
