6f3f8b364f6b3bbd25841d76f8d8e32301280c68
Kien Dinh <kien.dinh@asnet.com.vn>
2025-07-03 18:07:21 +0700

    feat: update navbar header

--- nextjs-practices/practice-one/src/layouts/Header/header.tsx
+++ nextjs-practices/practice-one/src/layouts/Header/header.tsx
@@ -2,8 +2,8 @@ import { Logo } from '@/components/common/ui/logo';
 import Navbar from './Navbar/Navbar';
 
 const Header = () => (
-  <header className="w-full border-b bg-white">
-    <div className="container mx-auto flex items-center justify-between py-3 px-2 md:px-0">
+  <header className="w-full bg-white">
+    <div className="container mx-auto flex items-center justify-between py-3 px-2 md:px-0 lg:py-[41px]">
       <Logo href="/" src="/images/logo.svg" />
       <Navbar />
     </div>
