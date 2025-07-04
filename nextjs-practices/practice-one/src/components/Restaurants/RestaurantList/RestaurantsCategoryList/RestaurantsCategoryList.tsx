8b2e707f81245e6fc87dbb465b7b40084471de5f
Kien Dinh <kien.dinh@asnet.com.vn>
2025-07-04 18:14:21 +0700

    feat: implement the restaurants list component

---
+++ nextjs-practices/practice-one/src/components/Restaurants/RestaurantList/RestaurantsCategoryList/RestaurantsCategoryList.tsx
@@ -0,0 +1,33 @@
+import { Heading } from '@/components/common/ui/heading';
+import RestaurantsCard from '../RestaurantsCard/RestaurantsCard';
+import { Product } from '@/types/product';
+
+interface RestaurantsCategorySectionProps {
+  category: string;
+  products: Product[];
+}
+
+const RestaurantsCategoryList = ({
+  category,
+  products,
+}: RestaurantsCategorySectionProps) => (
+  <div>
+    <Heading size="lg" className="text-primary mb-[40px]">
+      {category}
+    </Heading>
+    <div className="grid grid-cols-1 gap-[20px] lg:grid-cols-2 2xl:grid-cols-3">
+      {products.slice(0, 6).map((product) => (
+        <RestaurantsCard
+          key={product.name + product.id}
+          id={product.id}
+          name={product.name}
+          description={product.description}
+          price={product.price}
+          image={product.image}
+        />
+      ))}
+    </div>
+  </div>
+);
+
+export default RestaurantsCategoryList;
