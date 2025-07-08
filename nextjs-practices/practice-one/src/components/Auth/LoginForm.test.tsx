7c742ff21de27bd523a7b636e969cfb0f4f53e53
Kien Dinh <kien.dinh@asnet.com.vn>
2025-07-08 09:31:00 +0700

    feat: update unit test for login form

--- nextjs-practices/practice-one/src/components/Auth/LoginForm.test.tsx
+++ nextjs-practices/practice-one/src/components/Auth/LoginForm.test.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import LoginForm from './LoginForm';
 import { signIn } from 'next-auth/react';
