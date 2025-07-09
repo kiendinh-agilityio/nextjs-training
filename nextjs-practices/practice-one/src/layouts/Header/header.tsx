c42f5ff43a42d52ac2542601bb024e2d43e11438
Kien Dinh <kien.dinh@asnet.com.vn>
2025-07-09 17:58:07 +0700

    feat: update ui for Header

--- nextjs-practices/practice-one/src/layouts/Header/header.tsx
+++ nextjs-practices/practice-one/src/layouts/Header/header.tsx
@@ -1,17 +1,15 @@
 import { Logo } from '@/components/common/ui/logo';
 import Navbar from './Navbar/Navbar';
-import { cn } from '@/lib/utils';
+import HeaderInfoBar from './HeaderInfoBar/HeaderInfoBar';
 
 const Header = () => (
   <header className="w-full bg-white">
-    <div
-      className={cn(
-        'container mx-auto flex items-center justify-between',
-        'px-2 py-3 md:px-0 lg:py-[41px]',
-      )}
-    >
-      <Logo href="/" src="/images/logo.svg" />
-      <Navbar />
+    <div className="container mx-auto flex flex-col-reverse gap-3 md:gap-0 md:px-0 lg:flex-col">
+      <HeaderInfoBar />
+      <div className="flex items-center justify-between py-3 lg:py-[41px]">
+        <Logo href="/" src="/images/logo.svg" />
+        <Navbar />
+      </div>
     </div>
   </header>
 );
