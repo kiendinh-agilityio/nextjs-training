3af5ce98efb181421da907864e15b72bdae5fff5
Kien Dinh <kien.dinh@asnet.com.vn>
2025-07-10 18:12:03 +0700

    feat: update logic for Header Info Bar

--- nextjs-practices/practice-one/src/layouts/Header/HeaderInfoBar/HeaderInfoBar.tsx
+++ nextjs-practices/practice-one/src/layouts/Header/HeaderInfoBar/HeaderInfoBar.tsx
@@ -1,10 +1,28 @@
+'use client';
+
 import { cn } from '@/lib/utils';
 
 import LocationIcon from '@/components/Icons/LocationIcon';
 import CartIcon from '@/components/Icons/CartIcon';
-import Link from 'next/link';
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
+import { ROUTERS } from '@/constants/router';
+import { useCartStore } from '@/stores/useCartStore';
 
 const HeaderInfoBar = () => {
+  const { items } = useCartStore();
+  const cartCount = items.reduce((sum, item) => sum + item.quantity, 0);
+  const { data: session } = useSession();
+  const router = useRouter();
+
+  const handleCartClick = () => {
+    if (session?.user?.email) {
+      router.push(ROUTERS.CART);
+    } else {
+      router.push(ROUTERS.LOGIN);
+    }
+  };
+
   return (
     <div
       className={cn(
@@ -24,16 +42,30 @@ const HeaderInfoBar = () => {
         </p>
         <p className="font-bold text-primary">Change Location</p>
       </div>
-      <Link
-        href="/cart"
+      <button
+        type="button"
         className={cn(
           'absolute right-0 top-0 flex w-full items-center justify-center sm:w-auto',
           'rounded-xl bg-success lg:rounded-b-xl lg:rounded-t-none',
           'px-[37px] pb-[12px] pt-[15px]',
         )}
+        onClick={handleCartClick}
       >
-        <CartIcon className="lg:[w-43px] h-[43px]" />
-      </Link>
+        <div className="relative">
+          <CartIcon className="lg:[w-43px] h-[43px]" />
+          {cartCount > 0 && (
+            <span
+              className={cn(
+                'absolute -right-2 -top-2 flex h-6 min-h-[24px] w-6 min-w-[24px] items-center justify-center',
+                'rounded-full border-2 border-white bg-primary shadow-lg',
+                'text-xs font-bold text-white',
+              )}
+            >
+              {cartCount}
+            </span>
+          )}
+        </div>
+      </button>
     </div>
   );
 };
