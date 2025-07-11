b47176f5cc813046d1431dd381b36e2ae663eb50
Kien Dinh <kien.dinh@asnet.com.vn>
2025-07-11 15:32:34 +0700

    feat: update logic for Cart List component

--- nextjs-practices/practice-one/src/components/Cart/CartList/CartList.tsx
+++ nextjs-practices/practice-one/src/components/Cart/CartList/CartList.tsx
@@ -1,19 +1,28 @@
-import React from 'react';
+'use client';
+
+import React, { useOptimistic, useTransition } from 'react';
 
 import { useCartStore } from '@/stores/useCartStore';
+import { CartItem } from '@/types/cart';
 import CartEmpty from '../CartEmpty/CartEmpty';
-import CartItem from './CartItem/CartItem';
+import CartItemComponent from './CartItem/CartItem';
 
 const CartList = () => {
   const { items } = useCartStore();
-  if (!items.length) return <CartEmpty />;
+  const [optimisticItems] = useOptimistic(items);
+  const [isTransitioning] = useTransition();
+
+  // Use optimistic items when there's a transition
+  const displayItems = isTransitioning ? optimisticItems : items;
+
+  if (!displayItems.length) return <CartEmpty />;
 
   return (
     <div className="w-full rounded-3xl border bg-white p-6">
-      {items.map((item, idx) => (
+      {displayItems.map((item: CartItem, idx: number) => (
         <React.Fragment key={item.id}>
-          <CartItem item={item} />
-          {idx < items.length - 1 && <div className="my-6 border-t" />}
+          <CartItemComponent item={item} />
+          {idx < displayItems.length - 1 && <div className="my-6 border-t" />}
         </React.Fragment>
       ))}
     </div>
