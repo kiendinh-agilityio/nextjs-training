c5989773a9e6140f23035c94e3966c826eb05a6b
Kien Dinh <kien.dinh@asnet.com.vn>
2025-07-11 15:33:46 +0700

    feat: implement ui for cart skeleton component

---
+++ nextjs-practices/practice-one/src/components/Cart/CartSkeleton/CartListSkeleton.tsx
@@ -0,0 +1,7 @@
+const CartListSkeleton = () => (
+  <div className="flex min-h-[180px] w-full animate-pulse items-center justify-center rounded-3xl border bg-white p-6">
+    <div className="h-40 w-full rounded-2xl bg-gray-100" />
+  </div>
+);
+
+export default CartListSkeleton;
---
+++ nextjs-practices/practice-one/src/components/Cart/CartSkeleton/index.ts
@@ -0,0 +1,2 @@
+export { default as CartFormSkeleton } from './CartFormSkeleton';
+export { default as CartListSkeleton } from './CartListSkeleton';
