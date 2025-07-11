d01149c5d324177756e6904e419de7892dd63a8c
Kien Dinh <kien.dinh@asnet.com.vn>
2025-07-11 09:29:24 +0700

    feat: update config for jest test

--- nextjs-practices/practice-one/jest.config.ts
+++ nextjs-practices/practice-one/jest.config.ts
@@ -21,8 +21,13 @@ const config: Config = {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
   transformIgnorePatterns: [
-    '/node_modules/(?!(next-auth|@next|next|@babel|react|sonner|lucide-react)/)',
+    'node_modules/(?!(next-auth|@next|next|react|sonner|lucide-react|zustand)/)',
   ],
+  testMatch: [
+    '<rootDir>/src/**/__tests__/**/*.{js,jsx,ts,tsx}',
+    '<rootDir>/src/**/*.{test,spec}.{js,jsx,ts,tsx}',
+  ],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
