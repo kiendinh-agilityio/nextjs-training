bf34f4bcca1d22fe67d55c8f4a19bb1c2b217168
Kien Dinh <kien.dinh@asnet.com.vn>
2025-07-11 15:32:02 +0700

    feat: implement Cart Content section

---
+++ nextjs-practices/practice-one/src/components/Cart/CartContent/CartContent.tsx
@@ -0,0 +1,43 @@
+'use client';
+
+import { Suspense } from 'react';
+import { useCartStore } from '@/stores/useCartStore';
+import {
+  CartContainer,
+  CartLayout,
+  CartList,
+  CartForm,
+  CartEmpty,
+} from '@/components/Cart';
+import {
+  CartListSkeleton,
+  CartFormSkeleton,
+} from '@/components/Cart/CartSkeleton';
+
+const CartContent = () => {
+  const { subTotal, discount, total, items, _hasHydrated } = useCartStore();
+
+  if (!_hasHydrated) {
+    return (
+      <CartContainer>
+        <CartLayout>
+          <CartListSkeleton />
+          <CartFormSkeleton />
+        </CartLayout>
+      </CartContainer>
+    );
+  }
+
+  return (
+    <CartContainer>
+      <CartLayout>
+        <Suspense fallback={<div>Loading cart...</div>}>
+          {items.length ? <CartList /> : <CartEmpty />}
+        </Suspense>
+        <CartForm subTotal={subTotal} discount={discount} total={total} />
+      </CartLayout>
+    </CartContainer>
+  );
+};
+
+export default CartContent;
