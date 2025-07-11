2074c5737cdfb3602288cf8c6ffb5ab81be31346
Kien Dinh <kien.dinh@asnet.com.vn>
2025-07-11 15:27:50 +0700

    feat: update actions cart

--- nextjs-practices/practice-one/src/actions/cart.ts
+++ nextjs-practices/practice-one/src/actions/cart.ts
@@ -1,10 +1,28 @@
 'use server';
 
 import { revalidatePath } from 'next/cache';
-import { CartActionPayload } from '@/types/cart';
+import { CartActionPayload, CartState } from '@/types/cart';
 
-export const cartAction = async (_action: CartActionPayload) => {
-  revalidatePath('/cart');
-
-  return { success: true };
+const initialState: CartState = {
+  items: [],
+  subTotal: 0,
+  discount: 0,
+  total: 0,
 };
+
+export const cartAction = async (
+  prevState: CartState,
+  _action: CartActionPayload,
+): Promise<CartState> => {
+  try {
+    revalidatePath('/cart');
+    revalidatePath('/');
+
+    return prevState;
+  } catch (error) {
+    throw new Error('Failed to update cart: ' + (error as Error).message);
+  }
+};
+
+// Server action to get cart state from client
+export const getCartStateAction = async (): Promise<CartState> => initialState;
