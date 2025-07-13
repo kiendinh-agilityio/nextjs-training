82b7937484ceecc7e9f493c923dc958632d5285c
Kien Dinh <kien.dinh@asnet.com.vn>
2025-07-13 20:24:28 +0700

    feat: update ui and logic for restaurant list section

--- nextjs-practices/practice-one/src/components/Restaurants/RestaurantList/RestaurantListSection.tsx
+++ nextjs-practices/practice-one/src/components/Restaurants/RestaurantList/RestaurantListSection.tsx
@@ -1,11 +1,13 @@
 'use client';
 
 import { useEffect, useState } from 'react';
-import { Product } from '@/types/product';
 import { getRestaurantList } from '@/actions/product';
+import { Product } from '@/types/product';
+import { CATEGORIES_ITEM } from '@/constants/restaurants-data';
 
 import RestaurantsCategoryList from './RestaurantsCategoryList/RestaurantsCategoryList';
 import CategorySkeletonSection from '@/components/ProductSkeleton/CategorySkeleton';
+import LoadMore from '@/components/LoadMore/LoadMore';
 
 // Accept category as a prop
 interface RestaurantListSectionProps {
@@ -21,7 +23,6 @@ const RestaurantListSection = ({ category }: RestaurantListSectionProps) => {
 
     getRestaurantList(!category || category === 'Offers' ? undefined : category)
       .then((res) => setData(res))
-
       .finally(() => setLoading(false));
   }, [category]);
 
@@ -48,18 +49,34 @@ const RestaurantListSection = ({ category }: RestaurantListSectionProps) => {
   const categories: [string, Product[]][] = Array.from(
     data.reduce((map: Map<string, Product[]>, item) => {
       if (!map.has(item.category)) map.set(item.category, []);
-
       map.get(item.category)!.push(item);
-
       return map;
     }, new Map()),
   );
 
+  const orderedCategories = CATEGORIES_ITEM.filter(
+    (cat) => cat !== 'Offers',
+  ).map(
+    (cat) =>
+      [cat, categories.find(([c]) => c === cat)?.[1] || []] as [
+        string,
+        Product[],
+      ],
+  );
+
+  const renderCategoryList = ([category]: [string, Product[]]) => (
+    <RestaurantsCategoryList key={category} category={category} />
+  );
+
   return (
     <section className="container mx-auto flex flex-col gap-32 sm:px-0">
-      {categories.map(([category]) => (
-        <RestaurantsCategoryList key={category} category={category} />
-      ))}
+      <LoadMore
+        items={orderedCategories}
+        initialCount={3}
+        step={3}
+        renderItem={renderCategoryList}
+        className="flex flex-col gap-32"
+      />
     </section>
   );
 };
