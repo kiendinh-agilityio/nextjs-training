a0f408cf9ca3cbf04290c0683b15c9421d385408
Kien Dinh <kien.dinh@asnet.com.vn>
2025-07-14 16:26:55 +0700

    feat: add metadata for product detail page

--- nextjs-practices/practice-one/src/app/(restaurants)/product-detail/[id]/page.tsx
+++ nextjs-practices/practice-one/src/app/(restaurants)/product-detail/[id]/page.tsx
@@ -4,6 +4,18 @@ import {
   ProductRelatedSection,
   ProductDetailContent,
 } from '@/components/ProductDetail';
+import { createMetadata } from '@/utils/metadata';
+import { BASE_URL } from '@/constants/url';
+import { ROUTERS } from '@/constants/router';
+
+export const metadata = createMetadata({
+  title: 'Product Detail Page',
+  description:
+    'Explore detailed product information, mouth-watering dishes, and exclusive offers on Order.uk. Check ingredients, reviews, and order your favorite meal today',
+  keywords: ['restaurant', 'products detail', 'Order.uk'],
+  url: `${BASE_URL}${ROUTERS.PRODUCT_DETAIL}`,
+  imageAlt: 'Order.uk Product Detail',
+});
 
 interface ProductDetailPageProps {
   params: Promise<{ id: string }>;
