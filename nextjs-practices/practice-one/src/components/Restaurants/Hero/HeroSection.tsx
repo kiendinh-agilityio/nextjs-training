0f0e7d103857a4bb91eb3d7e8cc421f2187d2917
Kien Dinh <kien.dinh@asnet.com.vn>
2025-08-04 16:42:11 +0700

    fix: update styles for Hero section

--- nextjs-practices/practice-one/src/components/Restaurants/Hero/HeroSection.tsx
+++ nextjs-practices/practice-one/src/components/Restaurants/Hero/HeroSection.tsx
@@ -15,7 +15,7 @@ const HeroSection = () => (
       className={cn(
         'container flex flex-col-reverse',
         'pb-[49px] pt-[48px] sm:px-0',
-        'lg:flex-row lg:justify-between lg:gap-[81px] lg:pb-[51px] lg:pt-[57px]',
+        'lg:flex-row lg:justify-between lg:gap-[65px] lg:pb-[51px] lg:pt-[57px] 2xl:gap-[81px]',
         'xl:px-[57px]',
       )}
     >
