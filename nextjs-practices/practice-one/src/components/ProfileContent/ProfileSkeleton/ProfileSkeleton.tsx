fcd277a13067733a4c6949801e8e09e09bf5d6cb
Kien Dinh <kien.dinh@asnet.com.vn>
2025-08-05 15:49:46 +0700

    fix: update theme class for Profile skeleton section

--- nextjs-practices/practice-one/src/components/ProfileContent/ProfileSkeleton/ProfileSkeleton.tsx
+++ nextjs-practices/practice-one/src/components/ProfileContent/ProfileSkeleton/ProfileSkeleton.tsx
@@ -4,7 +4,7 @@ const ProfileSkeleton = () => (
   <div className="container mx-auto flex min-h-screen flex-col items-start pt-12 sm:px-0">
     <div className="mx-auto flex w-full max-w-3xl flex-col overflow-hidden rounded-xl bg-white shadow-base">
       {/* Skeleton Header */}
-      <div className="flex w-full flex-col items-center justify-between rounded-t-xl bg-[#0b163f] px-8 pb-6 pt-8 md:flex-row">
+      <div className="flex w-full flex-col items-center justify-between rounded-t-xl bg-navy px-8 pb-6 pt-8 md:flex-row">
         <div className="relative mb-4 h-24 w-24 overflow-hidden rounded-full border-4 border-orange-400 shadow-lg">
           <Skeleton className="h-full w-full rounded-full" />
         </div>
@@ -14,7 +14,7 @@ const ProfileSkeleton = () => (
         </div>
       </div>
       {/* Skeleton Info + Actions */}
-      <div className="flex flex-col items-center bg-[#F8F9FB] px-8 pb-8">
+      <div className="flex flex-col items-center bg-gray-270 px-8 pb-8">
         <div className="flex w-full justify-center">
           <article className="mt-10 min-w-[280px] flex-1 rounded-xl bg-white p-6 shadow-base">
             <Skeleton className="mb-4 h-6 w-48 rounded" />
