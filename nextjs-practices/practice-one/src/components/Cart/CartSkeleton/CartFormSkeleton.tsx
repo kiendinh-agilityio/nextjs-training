7b352fd9a4ed289d66050ecd7efdf564ecb4da68
Kien Dinh <kien.dinh@asnet.com.vn>
2025-08-05 15:52:39 +0700

    fix: update theme class for Cart Form Skeleton

--- nextjs-practices/practice-one/src/components/Cart/CartSkeleton/CartFormSkeleton.tsx
+++ nextjs-practices/practice-one/src/components/Cart/CartSkeleton/CartFormSkeleton.tsx
@@ -27,7 +27,7 @@ const CartFormSkeleton = () => (
         disabled
       />
       <button
-        className="rounded-lg bg-[#a3a3a3] px-6 py-2 font-medium text-white"
+        className="rounded-lg bg-gray-400 px-6 py-2 font-medium text-white"
         disabled
       >
         Apply
