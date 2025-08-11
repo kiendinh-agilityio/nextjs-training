318f5735ac0d6a1666f3e030a9486969f58be4e4
Kien Dinh <kien.dinh@asnet.com.vn>
2025-08-11 10:31:00 +0700

    feat: update unit test for LoginPage

--- nextjs-practices/practice-one/src/app/(auth)/login/LoginPage.test.tsx
+++ nextjs-practices/practice-one/src/app/(auth)/login/LoginPage.test.tsx
@@ -28,4 +28,23 @@ describe('LoginPage', () => {
     const { asFragment } = render(<LoginPage />);
     expect(asFragment()).toMatchSnapshot();
   });
+
+  it('should render heading with correct text', () => {
+    const { getByTestId } = render(<LoginPage />);
+    const heading = getByTestId('heading-mock');
+    expect(heading).toBeInTheDocument();
+  });
+
+  it('should render login form', () => {
+    const { getByTestId } = render(<LoginPage />);
+    const loginForm = getByTestId('login-form-mock');
+    expect(loginForm).toBeInTheDocument();
+  });
+
+  it('should render all required components', () => {
+    const { getByTestId } = render(<LoginPage />);
+
+    expect(getByTestId('heading-mock')).toBeInTheDocument();
+    expect(getByTestId('login-form-mock')).toBeInTheDocument();
+  });
 });
